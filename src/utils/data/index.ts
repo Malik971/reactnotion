6dae3e8c7dcd7440fef5c0fe187cdb8de038d6d9
Malik Ibo <malik97un@gmail.com>
2025-06-09 07:09:37 +0200

    trie des fichers

---
+++ src/utils/data/index.ts
@@ -0,0 +1,105 @@
+const USERS = [
+  {
+    "id": "68466b4a64bdaec5e54a2460",
+    "guid": "107a19a5-c05a-4e1d-8a45-2b0bcbc670b5",
+    "picture": "http://placehold.it/32x32",
+    "firstName": "Nielsen",
+    "surname": "Powell",
+    "gender": "male",
+    "company": "VERTON",
+    "email": "nielsenpowell@verton.com",
+    "phone": "+1 (840) 483-3688",
+    "address": "845 Varick Street, Wawona, Wisconsin, 8717",
+    "about": "minim sint tempor dolor consequat mollit ex nulla excepteur duis"
+  },
+  {
+    "id": "68466b4a28e3eb619a3496eb",
+    "guid": "2edf91de-e400-43fc-9f66-846e9baa0b90",
+    "picture": "http://placehold.it/32x32",
+    "firstName": "Smith",
+    "surname": "Murray",
+    "gender": "male",
+    "company": "GEEKFARM",
+    "email": "smithmurray@geekfarm.com",
+    "phone": "+1 (887) 534-3303",
+    "address": "164 Applegate Court, Rosburg, West Virginia, 2344",
+    "about": "minim do quis tempor elit ut eiusmod mollit nostrud ullamco"
+  },
+  {
+    "id": "68466b4af4b8b5b937115ef3",
+    "guid": "3782c4ce-659c-4cf4-96fb-15b4da88bdf7",
+    "picture": "http://placehold.it/32x32",
+    "firstName": "Pearlie",
+    "surname": "Mcintosh",
+    "gender": "female",
+    "company": "COMSTRUCT",
+    "email": "pearliemcintosh@comstruct.com",
+    "phone": "+1 (884) 477-2737",
+    "address": "247 Oxford Walk, Lydia, Maryland, 3135",
+    "about": "voluptate quis tempor do pariatur nostrud velit culpa eiusmod esse"
+  },
+  {
+    "id": "68466b4a9a695af852489743",
+    "guid": "7eaf5c6d-e2d5-424b-af44-30f46b82f723",
+    "picture": "http://placehold.it/32x32",
+    "firstName": "House",
+    "surname": "Paul",
+    "gender": "male",
+    "company": "ZIORE",
+    "email": "housepaul@ziore.com",
+    "phone": "+1 (901) 445-3407",
+    "address": "784 Hampton Avenue, Forbestown, Kentucky, 5454",
+    "about": "reprehenderit ex commodo ad dolore nisi in voluptate reprehenderit ullamco"
+  },
+  {
+    "id": "68466b4a69374b63dffd88f0",
+    "guid": "27f29348-8b40-429c-bb21-5fb7b1fd9070",
+    "picture": "http://placehold.it/32x32",
+    "firstName": "Chapman",
+    "surname": "Gilmore",
+    "gender": "male",
+    "company": "EXOSWITCH",
+    "email": "chapmangilmore@exoswitch.com",
+    "phone": "+1 (858) 564-2733",
+    "address": "809 Canda Avenue, Ladera, Hawaii, 9857",
+    "about": "reprehenderit minim cillum et tempor eiusmod esse laborum anim esse"
+  },
+  {
+    "id": "68466b4a3b9660cf4523c370",
+    "guid": "385c24aa-cd0d-45a3-ae54-aa85a64171db",
+    "picture": "http://placehold.it/32x32",
+    "firstName": "Paula",
+    "surname": "Kelley",
+    "gender": "female",
+    "company": "INEAR",
+    "email": "paulakelley@inear.com",
+    "phone": "+1 (919) 511-3340",
+    "address": "787 Polar Street, Hailesboro, New Mexico, 8373",
+    "about": "consectetur est officia dolore Lorem dolore eiusmod aute non quis"
+  }
+]
+
+const MESSAGE = [
+  {
+    "id": "6844a802be81beefb1e455ab",
+    "texte": "proident consectetur sit quis excepteur reprehenderit tempor pariatur eu ullamco ex consequat reprehenderit incididunt sit sint duis Lorem Lorem mollit eiusmod magna enim officia pariatur esse non est id veniam"
+  },
+  {
+    "id": "6844a802851e9529a75c6631",
+    "texte": "cupidatat incididunt ex elit voluptate enim consectetur ad proident fugiat aliqua nostrud labore aliqua exercitation incididunt exercitation adipisicing occaecat ex amet fugiat dolore et cillum eiusmod officia in consequat minim"
+  },
+  {
+    "id": "6844a802c36be5aa2edb16fe",
+    "texte": "magna culpa sunt ipsum voluptate voluptate culpa nostrud laboris deserunt et anim aliqua commodo commodo id consequat proident excepteur voluptate mollit anim cupidatat adipisicing cillum ad eiusmod duis officia ut"
+  },
+  {
+    "id": "6844a802e115bf9b0744b6e5",
+    "texte": "nostrud amet culpa pariatur id sit ullamco sit ad consectetur velit ex fugiat magna ipsum id enim ex anim sunt laborum mollit qui officia minim eu aliquip cillum mollit ut"
+  },
+  {
+    "id": "6844a8026eef1427634e7901",
+    "texte": "dolore exercitation proident cillum cillum esse officia non veniam dolor reprehenderit ad minim amet esse nostrud qui voluptate ad tempor cillum veniam eiusmod sint culpa sunt dolor incididunt ullamco ullamco"
+  }
+]
+
+export { USERS, MESSAGE };
