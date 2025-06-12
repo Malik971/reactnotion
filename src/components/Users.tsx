5b4732f18b458d6e71bb62ad6d8ad096cb7cf996
Malik Ibo <malik97un@gmail.com>
2025-06-12 20:41:12 +0200

    Trie alphabetique v.1.0

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -19,13 +19,14 @@ type Props = {
 
 function Users({ users }: Props) {
   const [sortedUsers, setSortedUsers] = useState(users);
+  const [reverseOrdre, setReverseOrdre] = useState(1);
   const sortBy = (field: string, valeur: string) => {
     const result = sortedUsers.sort((first: UsersProps, second: UsersProps) => {
       const firstValue = first[field as keyof UsersProps] || "";
       const secondValue = second[field as keyof UsersProps] || "";
-      
+
       let order = 1;
-      if (valeur === "femele") {
+      if (valeur === "femele" || valeur === "-1") {
         order = -1;
       }
 
@@ -36,52 +37,74 @@ function Users({ users }: Props) {
     });
     setSortedUsers([...result]);
   };
-    return (
-      <section>
-        <div className="flex bg-gray-200 justify-end items-center p-4 my-2 text-sm rounded-lg shadow-md gap-2">
-          <span>Trier par</span>
-          <button
-            type="button"
-            className="border border-gray-100 bg-gray-50 rounded-md px-2 py-1 text-xs"
-            onClick={() => sortBy("gender", "femele")}
-          >
-            Femmes
-          </button>
-          <button
-            type="button"
-            className="border border-gray-100 bg-gray-50 rounded-md px-2 py-1 text-xs"
-            onClick={() => sortBy("gender", "male")}
-          >
-            Hommes
-          </button>
-        </div>
-        <div className="grid md:grid-cols-2 gap-4">
-          {sortedUsers.map(
-            ({
-              id,
-              firstName,
-              surname,
-              email = "Inconnu", // Default value for email
-              phone,
-              about,
-              gender,
-            }: UsersProps) => (
-              <article
-                className="bg-white border border-gray-300 text-sm rounded-lg p-4 shadow-md"
-                key={id}
-              >
-                <h3 className="text-lg">
-                  {gender} {firstName} {surname}
-                </h3>
-                <p>{email}</p>
-                <p>{phone}</p>
-                <p>{about}</p>
-              </article>
-            )
-          )}
-        </div>
-      </section>
-    );
+  const sortReverse = () => {
+    const result = sortedUsers.sort((first: UsersProps, second: UsersProps) => {
+      const firstValue = first.firstName;
+      const secondValue = second.firstName;
+      const order = reverseOrdre === 1 ? -1 : 1;
+      setReverseOrdre(order);
+
+
+      let sortResult = 0;
+      if (firstValue > secondValue) sortResult = 1;
+      if (firstValue < secondValue) sortResult = -1;
+      return sortResult * reverseOrdre;
+    });
+    setSortedUsers([...result]);
+  };
+  return (
+    <section>
+      <div className="flex bg-gray-200 justify-end items-center p-4 my-2 text-sm rounded-lg shadow-md gap-2">
+        <span>Trier par</span>
+        <button
+          type="button"
+          className="border border-red-400 bg-gray-50 rounded-md px-2 py-1 text-xs"
+          onClick={() => sortBy("gender", "femele")}
+        >
+          Femmes
+        </button>
+        <button
+          type="button"
+          className="border border-gray-100 bg-gray-50 rounded-md px-2 py-1 text-xs"
+          onClick={() => sortBy("gender", "male")}
+        >
+          Hommes
+        </button>
+        <button
+          type="button"
+          className="border border-blue-400 bg-gray-50 rounded-md px-2 py-1 text-xs"
+          onClick={() => sortReverse()}
+        >
+          Reverse
+        </button>
+      </div>
+      <div className="grid md:grid-cols-2 gap-4">
+        {sortedUsers.map(
+          ({
+            id,
+            firstName,
+            surname,
+            email = "Inconnu", // Default value for email
+            phone,
+            about,
+            gender,
+          }: UsersProps) => (
+            <article
+              className="bg-white border border-gray-300 text-sm rounded-lg p-4 shadow-md"
+              key={id}
+            >
+              <h3 className="text-lg">
+                {gender} {firstName} {surname}
+              </h3>
+              <p>{email}</p>
+              <p>{phone}</p>
+              <p>{about}</p>
+            </article>
+          )
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default Users;
